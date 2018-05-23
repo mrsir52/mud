556296ba63966d17a49797251014a79ccd472851
Manny <coachmannytw@gmail.com>
2018-05-23 07:42:04 -0600

    new front end demo

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+
+                <div>
+                    <iframe src="https://www.youtube.com/embed/J22sBd7LgGY?rel=0&amp;showinfo=0&autoplay=1&loop=1"
+                            allow="autoplay; encrypted-media">
+                    </iframe>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
